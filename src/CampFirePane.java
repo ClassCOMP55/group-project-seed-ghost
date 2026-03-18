6af09880f8f8cdbb86ae18753c11c6f04d359df6
Lance Vang <l_vang21@u.pacific.edu>
2026-03-18 10:34:01 -0700

    created a CampFirePane

--- src/CampFirePane.java
+++ src/CampFirePane.java
@@ -1,8 +1,4 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import acm.graphics.*;
 
-public class CampFirePane extends GraphicsPane{
+public class CampFirePane {
 
 }
