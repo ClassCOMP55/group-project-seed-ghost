a8aea34dd4777f31892e9f92152c997f2758d569
Lance Vang <l_vang21@u.pacific.edu>
2026-03-19 01:23:09 -0700

    I've added quite a lot from lines 7 to 37

--- src/ShopPane.java
+++ src/ShopPane.java
@@ -3,7 +3,9 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import acm.graphics.*;
 
+//The ShopPane
 public class ShopPane extends GraphicsPane {
-
+	private Player player;
+	private string 
 }
 //work in progress
