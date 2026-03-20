1a3ccb45e6a9485a31012a7ac4eb019af8175fd1
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-20 11:56:46 -0700

    Added mouseClicked method to MapPane

--- src/MapPane.java
+++ src/MapPane.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
@@ -65,5 +66,14 @@ public class MapPane extends GraphicsPane {
 		
 		
 	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(1)) {
+			mainScreen.switchToShopPane();
+		}
+	}
+		
+
 	
 }
