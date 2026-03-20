0401759194f15813aa2ed2feec370678ef7f104b
Lance Vang <l_vang21@u.pacific.edu>
2026-03-20 15:19:04 -0700

    created campfire variable and added campfire to main app

--- src/CampFirePane.java
+++ src/CampFirePane.java
@@ -1,8 +1,35 @@
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import acm.graphics.*;
 
 public class CampFirePane extends GraphicsPane{
+	public CampFirePane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
+	
+	@Override
+	public void showContent() {
+		addText();
+	}
+
+	@Override
+	public void hideContent() {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
+	
+	private void addText() {
+		GLabel title = new GLabel("CAMP", 100, 70);
+		title.setColor(Color.ORANGE);
+		title.setFont("DialogInput-PLAIN-80");
+		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 70);
+		
+		contents.add(title);
+		mainScreen.add(title);
+		}
 
 }
