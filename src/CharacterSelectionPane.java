466f74cc4b80f1216c4de6c894160a581d0b70c8
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-20 10:39:53 -0700

    Added mouseClicked() method to connect to map

--- src/CharacterSelectionPane.java
+++ src/CharacterSelectionPane.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
@@ -30,6 +31,22 @@ public class CharacterSelectionPane extends GraphicsPane {
 		
 		contents.add(title);
 		mainScreen.add(title);
+		
+		GLabel mapLabel = new GLabel("Go to Map", 100, 70);
+		mapLabel.setColor(Color.BLUE);
+		mapLabel.setFont("DialogInput-PLAIN-20");
+		mapLabel.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 270);
+		
+		contents.add(mapLabel);
+		mainScreen.add(mapLabel);
 	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(1)) {
+			mainScreen.switchToMapPane();
+		}
+	}
+		
 
 }
