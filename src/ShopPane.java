93fbf5bdb8742b8512db043770aed5e1e492ad7b
Lance Vang <l_vang21@u.pacific.edu>
2026-03-20 12:00:48 -0700

    Added constructor and other stuff

--- src/ShopPane.java
+++ src/ShopPane.java
@@ -7,5 +7,31 @@ import acm.graphics.*;
 
 //The ShopPane
 public class ShopPane extends GraphicsPane {
+	public ShopPane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
+	
+	@Override
+	public void showContent() {
+		addText();
+	}
+
+	@Override
+	public void hideContent() {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
+	
+	private void addText() {
+		GLabel title = new GLabel("SHOP", 100, 70);
+		title.setColor(Color.BLUE);
+		title.setFont("DialogInput-PLAIN-80");
+		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 70);
+		
+		contents.add(title);
+		mainScreen.add(title);
+		}
 }
 //work in progress
