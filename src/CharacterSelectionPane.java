b80a9c4ce2c6674f42a311f0f5cc17dacd8a0168
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-19 20:48:05 -0700

    Added constructor and basic methods for CharacterSelectionPane

--- src/CharacterSelectionPane.java
+++ src/CharacterSelectionPane.java
@@ -1,5 +1,21 @@
+import acm.graphics.GObject;
 
 public class CharacterSelectionPane extends GraphicsPane {
 
+	public CharacterSelectionPane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
+	
+	@Override
+	public void showContent() {
+	}
+
+	@Override
+	public void hideContent() {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
 
 }
