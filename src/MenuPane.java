208643ab0890a09023a5f378c53b74ee07429d06
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-19 20:10:03 -0700

    Added addText method to MenuPane with a placeholder menu title

--- src/MenuPane.java
+++ src/MenuPane.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ public class MenuPane extends GraphicsPane {
 	
 	@Override
 	public void showContent() {
-		
+		addText();
 	}
 
 	@Override
@@ -19,6 +20,18 @@ public class MenuPane extends GraphicsPane {
 			mainScreen.remove(item);
 		}
 		contents.clear();
+	}
+	
+	private void addText() {
+		GLabel title = new GLabel("FateBound", 100, 70);
+		title.setColor(Color.RED);
+		title.setFont("DialogInput-PLAIN-60");
+		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 70);
+		
+		contents.add(title);
+		mainScreen.add(title);
+		
+		
 	}
 
 }
