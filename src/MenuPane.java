6837bdd3d20d2b5f654adfbefa845890f782184f
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-19 20:34:06 -0700

    Added placeholder labels for all MenuPane buttons

--- src/MenuPane.java
+++ src/MenuPane.java
@@ -25,11 +25,35 @@ public class MenuPane extends GraphicsPane {
 	private void addText() {
 		GLabel title = new GLabel("FateBound", 100, 70);
 		title.setColor(Color.RED);
-		title.setFont("DialogInput-PLAIN-60");
+		title.setFont("DialogInput-PLAIN-80");
 		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 70);
 		
 		contents.add(title);
 		mainScreen.add(title);
+		
+		GLabel newGame = new GLabel("The start new game button goes here", 100, 70);
+		newGame.setColor(Color.BLUE);
+		newGame.setFont("DialogInput-PLAIN-20");
+		newGame.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 270);
+		
+		contents.add(newGame);
+		mainScreen.add(newGame);
+		
+		GLabel continueGame = new GLabel("The continue game button goes here", 100, 70);
+		continueGame.setColor(Color.GREEN);
+		continueGame.setFont("DialogInput-PLAIN-20");
+		continueGame.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 340);
+		
+		contents.add(continueGame);
+		mainScreen.add(continueGame);
+		
+		GLabel options = new GLabel("The options buttom goes here", 100, 70);
+		options.setColor(Color.YELLOW);
+		options.setFont("DialogInput-PLAIN-20");
+		options.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 410);
+		
+		contents.add(options);
+		mainScreen.add(options);
 		
 		
 	}
