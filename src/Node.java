96e9b1557a9208675220ebe8b98a0d3e1a426f7f
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-21 21:06:30 -0700

    getRandomType() method,constructor, and instance variables for Node

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,26 @@
+import acm.graphics.GObject;
 
 public class Node {
+String type;
+int[] accessibleNodes = {1, 2};
+
+public Node(int i) {
+}
+
+public String getRandomType() {
+	
+	int randomNum = Chance.range(0, 2);
+	
+	switch(randomNum) {
+	case 0: return "Shop";
+	case 1: return "CampFire";
+	case 2: return "Loot";
+	}
+	return "n/a";
+	
+}
+	
+	
+
 
 }
