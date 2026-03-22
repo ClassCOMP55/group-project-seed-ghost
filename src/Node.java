9e49528fe8854b9b0025a84f7fd1d1bc32aad9e4
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-21 21:36:56 -0700

    Created assignType() method for Node class

--- src/Node.java
+++ src/Node.java
@@ -5,6 +5,7 @@ String type;
 int[] accessibleNodes = {1, 2};
 
 public Node(int i) {
+	assignType(i);
 }
 
 public String getRandomType() {
@@ -17,6 +18,28 @@ public String getRandomType() {
 	case 2: return "Loot";
 	}
 	return "n/a";
+	
+}
+
+public void assignType(int i) {
+	
+	switch(i) {
+	case 0: type = "Combat";
+	case 1: type = getRandomType();
+	case 2: type = getRandomType();
+	case 3: type = "Combat";
+	case 4: type = "Combat";
+	case 5: type = "Combat";
+	case 6: type = getRandomType();
+	case 7: type = getRandomType();
+	case 8: type = getRandomType();
+	case 9: type = getRandomType();
+	case 10: type = getRandomType();
+	case 11: type = "Combat";
+	case 12: type = "Combat";
+	case 13: type = "Combat";
+	}
+	
 	
 }
 	
