d23e6988a6c73c0aa40b05b9e712b984d95bde60
Lance Vang <l_vang21@u.pacific.edu>
2026-03-22 02:40:00 -0700

    Created ShopItem class to help with Shop Pane still workin on shoppane

---
+++ src/ShopItem.java
@@ -0,0 +1,44 @@
+
+public class ShopItem {
+	private Object item;
+	private int price;
+	
+	public ShopItem(Object item, int price) {
+		this.item = item;
+		this.price = price; 
+		
+	}
+	
+	public Object getItem() {
+		return item; 
+	}
+	
+	public int getPrice() {
+		return price; 
+	}
+	
+	public String getDisplayName() {
+		if(item instanceof WeaponItem) {
+			WeaponItem w =(WeaponItem) item;
+			if (w.getAffinity().equals("null")) {
+                return capitalize(w.getType());
+            } else {
+                return capitalize(w.getType()) + " of " + capitalize(w.getAffinity());
+            }
+		}
+		if (item instanceof ArmorItem) {
+			return item.toString();
+	    }
+
+	    if (item instanceof Character) {
+	    	Character c = (Character) item;
+	    	return "Mercenary: " + capitalize(c.getProfession());
+	    }
+	    return item.toString();
+	    }
+	 private String capitalize(String str) {
+		 return str.substring(0,1).toUpperCase() + str.substring(1);
+	 }
+
+
+}
