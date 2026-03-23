175705bdce7f21913971689b76a50b55c924e310
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-23 12:12:57 -0700

    Fixed file path issue

--- src/Entity.java
+++ src/Entity.java
@@ -197,7 +197,7 @@ public class Entity {
 	}
 	
 	public void setSprite(String sprite) {
-		this.sprite = new GImage(sprite);
+		this.sprite = new GImage(sprite+".png");
 	}
 
 	public double[] getStatusLevels() {
