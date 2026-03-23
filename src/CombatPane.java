b5e93e51c7362660e4c308a8c057ffdc350f62a2
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-22 22:05:54 -0700

    Made addText() method for combat page

--- src/CombatPane.java
+++ src/CombatPane.java
@@ -11,6 +11,7 @@ public class CombatPane extends GraphicsPane{
 	
 	@Override
 	public void showContent() {
+		addText();
 	}
 
 	@Override
@@ -19,6 +20,24 @@ public class CombatPane extends GraphicsPane{
 			mainScreen.remove(item);
 		}
 		contents.clear();
+	}
+	
+	private void addText() {
+		GLabel title = new GLabel("Combat Page", 100, 70);
+		title.setColor(Color.RED);
+		title.setFont("DialogInput-PLAIN-50");
+		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 70);
+		
+		contents.add(title);
+		mainScreen.add(title);
+		
+		GLabel returnLabel = new GLabel("Return button", 100, 70);
+		returnLabel.setColor(Color.BLACK);
+		returnLabel.setFont("DialogInput-PLAIN-20");
+		returnLabel.setLocation((mainScreen.getWidth() - returnLabel.getWidth()) / 2, 270);
+		
+		contents.add(returnLabel);
+		mainScreen.add(returnLabel);
 	}
 	
 
