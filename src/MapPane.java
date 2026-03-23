c01b48f2ffdb21555a71c6a6179e6e1e815f0f38
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-22 21:53:11 -0700

    Added drawLines() method for Map

--- src/MapPane.java
+++ src/MapPane.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GLabel;
+import acm.graphics.GLine;
 import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
@@ -26,6 +27,7 @@ public class MapPane extends GraphicsPane {
 		createPath();
 		createMap();
 	}
+	
 
 	@Override
 	public void hideContent() {
@@ -92,6 +94,7 @@ private void createMap() {
 		createRow(5,((mainScreen.getWidth() - 40) / 2)-200,260);
 		createRow(2,((mainScreen.getWidth() - 40) / 2)-50,160);
 		createRow(1,((mainScreen.getWidth() - 40) / 2),60);
+		drawLines();
 		
 	}
 	
@@ -105,6 +108,25 @@ private void createMap() {
 			mainScreen.add(node);
 			startX = startX+100;
 		}
+	}
+	
+	public void drawLines() {
+		for (int e = 0;e<myNodeObjects.size()-1;e++) {
+			GOval oval = myNodeObjects.get(e);
+			Node node = ovalToNode(oval);
+			int[] myArr = node.accessibleNodes;
+			
+			for (int i:myArr) {
+				GOval oval2 = myNodeObjects.get(i);
+				GLine line = new GLine(oval.getX()+20,oval.getY()+20,oval2.getX()+20,oval2.getY()+20);
+				contents.add(line);
+				mainScreen.add(line);
+				oval2.sendToFront();
+				
+			}
+			oval.sendToFront();
+		}
+	
 	}
 	
 	private void createBackground() {
