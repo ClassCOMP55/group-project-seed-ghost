0959b259429784ffd43404bbd471af5cb6490df0
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-22 22:39:52 -0700

    Added clickable character images that lead to Map

--- src/CharacterSelectionPane.java
+++ src/CharacterSelectionPane.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GRect;
@@ -15,6 +16,7 @@ public class CharacterSelectionPane extends GraphicsPane {
 	public void showContent() {
 		createBackground();
 		addText();
+		addCharacters();
 	}
 
 	@Override
@@ -27,35 +29,43 @@ public class CharacterSelectionPane extends GraphicsPane {
 	
 	private void addText() {
 		GLabel title = new GLabel("Choose your character!", 100, 70);
-		title.setColor(Color.RED);
+		title.setColor(Color.BLUE);
 		title.setFont("DialogInput-PLAIN-50");
 		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 70);
 		
 		contents.add(title);
 		mainScreen.add(title);
 		
-		GLabel mapLabel = new GLabel("Go to Map", 100, 70);
-		mapLabel.setColor(Color.BLUE);
-		mapLabel.setFont("DialogInput-PLAIN-20");
-		mapLabel.setLocation((mainScreen.getWidth() - mapLabel.getWidth()) / 2, 270);
-		
-		contents.add(mapLabel);
-		mainScreen.add(mapLabel);
 	}
 	
 	private void createBackground() {
 		GRect backGround = new GRect(800,600);
-		backGround.setColor(Color.DARK_GRAY);
-		backGround.setFillColor(Color.DARK_GRAY);
+		backGround.setColor(Color.BLACK);
+		backGround.setFillColor(Color.BLACK);
 		backGround.setFilled(true);
 		backGround.setLocation(0, 0);
 		contents.add(backGround);
 		mainScreen.add(backGround);
 	}
 	
+	private void addCharacters() {
+		GImage characterImage1 = new GImage("spr_Knight.png", 60, 250);
+		contents.add(characterImage1);
+		mainScreen.add(characterImage1);
+		
+		GImage characterImage2 = new GImage("spr_Samurai.png", 310, 250);
+		contents.add(characterImage2);
+		mainScreen.add(characterImage2);
+		
+		GImage characterImage3 = new GImage("spr_Sorcerer.png", 560, 250);
+		contents.add(characterImage3);
+		mainScreen.add(characterImage3);
+		
+	}
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(2)) {
+		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) instanceof GImage) {
 			mainScreen.switchToMapPane();
 		}
 	}
