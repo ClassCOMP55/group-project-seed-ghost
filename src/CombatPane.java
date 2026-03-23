22b2e665ec18ffd6760abf65242ed86f0d83f88b
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-03-23 00:19:57 -0700

    Adding CombatPane back because it apparently got deleted on github

---
+++ src/CombatPane.java
@@ -0,0 +1,57 @@
+
+import java.awt.Color;
+
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+
+public class CombatPane extends GraphicsPane{
+
+	public CombatPane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
+
+	@Override
+	public void showContent() {
+		createBackground();
+		addText();
+	}
+
+	@Override
+	public void hideContent() {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
+
+	private void addText() {
+		GLabel title = new GLabel("Combat Page", 100, 70);
+		title.setColor(Color.RED);
+		title.setFont("DialogInput-PLAIN-50");
+		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 70);
+
+		contents.add(title);
+		mainScreen.add(title);
+
+		GLabel returnLabel = new GLabel("Return button", 100, 70);
+		returnLabel.setColor(Color.BLACK);
+		returnLabel.setFont("DialogInput-PLAIN-20");
+		returnLabel.setLocation((mainScreen.getWidth() - returnLabel.getWidth()) / 2, 270);
+
+		contents.add(returnLabel);
+		mainScreen.add(returnLabel);
+	}
+
+	private void createBackground() {
+		GRect backGround = new GRect(800,600);
+		backGround.setColor(Color.DARK_GRAY);
+		backGround.setFillColor(Color.DARK_GRAY);
+		backGround.setFilled(true);
+		backGround.setLocation(0, 0);
+		contents.add(backGround);
+		mainScreen.add(backGround);
+	}
+	
+
+}
