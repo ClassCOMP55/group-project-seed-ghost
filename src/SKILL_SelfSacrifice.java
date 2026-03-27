3accad9474a23bba7f825ce3c6908a9407e7cf7e
cFranks2 <c_franks2@u.pacific.edu>
2026-03-27 11:32:22 -0700

    SelfSacrifice skill

---
+++ src/SKILL_SelfSacrifice.java
@@ -0,0 +1,25 @@
+
+public class SKILL_SelfSacrifice extends Skill {
+	private static final int SELF_DAMAGE = 50;
+	
+	public SKILL_SelfSacrifice () {
+		super(30);
+		setName("Self Sacrifice");
+		setName("Take " + SELF_DAMAGE + " damage to deal huge HOLY damage to a foe. Scales with FAITH and CONSTITUTION.");
+	}
+	
+	public void activationEffect (Entity me, Entity target) {
+		me.hurt(SELF_DAMAGE);
+		
+		double[] dmg = new double[] {0,0,0,0,0,0,0,0};
+		int scaling = (int) (me.getStatSpread()[EntityStats.FTH.ordinal()]*0.5 + me.getStatSpread()[EntityStats.CON.ordinal()]*0.5);
+		dmg[DamageType.HOLY.ordinal()] = 10 * Math.pow(1.04, scaling);
+		target.attackMe(dmg);
+		
+		me.drainMana(getManaCost());
+	}
+	
+	public boolean preconditionsMet (Entity self, Entity target) {
+		return self.getMana() >= getManaCost() && self.getHp() > SELF_DAMAGE;
+	}
+}
