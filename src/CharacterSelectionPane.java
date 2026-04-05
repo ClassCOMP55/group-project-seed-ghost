dce6ebd6d0a73972436b0ff332de905349d4baf6
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-04-04 18:54:46 -0700

    Character selection page now shows weapon type and stats

--- src/CharacterSelectionPane.java
+++ src/CharacterSelectionPane.java
@@ -79,7 +79,7 @@ public class CharacterSelectionPane extends GraphicsPane {
 		case "sorcerer": charImage = new GImage("spr_Sorcerer.png",50+(200*i),350); break;
 		case "paladin": charImage = new GImage("spr_Paladin.png",50+(200*i),350); break;
 		case "cleric": charImage = new GImage("spr_Cleric.png",50+(200*i),350); break;
-		case "ranger": charImage = new GImage("Ranger.png",50+(200*i),350); break;
+		case "ranger": charImage = new GImage("spr_RangerUpdated.png",50+(200*i),350);charImage.setSize(150, 150); break;
 		default: charImage = new GImage("spr_Marksman.png",50+(200*i),350); break;
 		}
 		charImage.setLocation(5+2*i*charImage.getWidth(), (600-charImage.getHeight())/2);
@@ -92,7 +92,7 @@ public class CharacterSelectionPane extends GraphicsPane {
 		for (Character myChar:myChars) {
 			int index = myChars.indexOf(myChar);
 			GImage image = myImages.get(index);
-			GRect box = new GRect(150,70);
+			GRect box = new GRect(150,185);
 			box.setLocation(image.getX(), image.getY()+image.getHeight());
 			box.setColor(Color.BLACK);
 			box.setFillColor(Color.BLACK);
@@ -108,18 +108,83 @@ public class CharacterSelectionPane extends GraphicsPane {
 			mainScreen.add(prof);
 			
 			GLabel health = new GLabel("Health: "+myChar.getHp());
-			health.setLocation(box.getX()+5, box.getY()+35);
+			health.setLocation(box.getX()+5, box.getY()+30);
 			health.setColor(Color.RED);
 			health.setFont("ARIEL-PLAIN-14");
 			contents.add(health);
 			mainScreen.add(health);
 			
 			GLabel mana = new GLabel("Mana: "+myChar.getManaMax());
-			mana.setLocation(box.getX()+5, box.getY()+55);
+			mana.setLocation(box.getX()+5, box.getY()+45);
 			mana.setColor(Color.RED);
 			mana.setFont("ARIEL-PLAIN-14");
 			contents.add(mana);
 			mainScreen.add(mana);
+			
+			GLabel weapon = new GLabel("Weapon: "+myChar.getWeapon().getType());
+			weapon.setLocation(box.getX()+5, box.getY()+60);
+			weapon.setColor(Color.RED);
+			weapon.setFont("ARIEL-PLAIN-14");
+			contents.add(weapon);
+			mainScreen.add(weapon);
+			
+			int[] stats = myChar.getStatSpread();
+			
+			GLabel str = new GLabel("Strength: "+stats[0]);
+			str.setLocation(box.getX()+5, box.getY()+75);
+			str.setColor(Color.RED);
+			str.setFont("ARIEL-PLAIN-14");
+			contents.add(str);
+			mainScreen.add(str);
+			
+			GLabel dex = new GLabel("Dexterity: "+stats[1]);
+			dex.setLocation(box.getX()+5, box.getY()+90);
+			dex.setColor(Color.RED);
+			dex.setFont("ARIEL-PLAIN-14");
+			contents.add(dex);
+			mainScreen.add(dex);
+			
+			GLabel prc = new GLabel("Precison: "+stats[2]);
+			prc.setLocation(box.getX()+5, box.getY()+105);
+			prc.setColor(Color.RED);
+			prc.setFont("ARIEL-PLAIN-14");
+			contents.add(prc);
+			mainScreen.add(prc);
+			
+			GLabel ist = new GLabel("Instinct: "+stats[3]);
+			ist.setLocation(box.getX()+5, box.getY()+120);
+			ist.setColor(Color.RED);
+			ist.setFont("ARIEL-PLAIN-14");
+			contents.add(ist);
+			mainScreen.add(ist);
+			
+			GLabel con = new GLabel("Constitution: "+stats[4]);
+			con.setLocation(box.getX()+5, box.getY()+135);
+			con.setColor(Color.RED);
+			con.setFont("ARIEL-PLAIN-14");
+			contents.add(con);
+			mainScreen.add(con);
+			
+			GLabel wil = new GLabel("Willpower: "+stats[5]);
+			wil.setLocation(box.getX()+5, box.getY()+150);
+			wil.setColor(Color.RED);
+			wil.setFont("ARIEL-PLAIN-14");
+			contents.add(wil);
+			mainScreen.add(wil);
+			
+			GLabel fth = new GLabel("Faith: "+stats[6]);
+			fth.setLocation(box.getX()+5, box.getY()+165);
+			fth.setColor(Color.RED);
+			fth.setFont("ARIEL-PLAIN-14");
+			contents.add(fth);
+			mainScreen.add(fth);
+			
+			GLabel arc = new GLabel("Arcane: "+stats[7]);
+			arc.setLocation(box.getX()+5, box.getY()+180);
+			arc.setColor(Color.RED);
+			arc.setFont("ARIEL-PLAIN-14");
+			contents.add(arc);
+			mainScreen.add(arc);
 			
 			
 		}
