50b280b1b8f38c6931419c3b470b779003d5a659
cFranks2 <c_franks2@u.pacific.edu>
2026-04-06 11:58:28 -0700

    SKILL_Taunt

--- src/Character.java
+++ src/Character.java
@@ -99,7 +99,7 @@ public class Character extends Entity {
 				tempSprite = "spr_Knight.png";
 				allowedArmors = new String[] {"light", "medium","heavy"};
 				
-				mySkills = new Skill[] {new SKILL_BasicAttack(), new SKILL_Guard(), new SKILL_IronWave(), new SKILL_Drain()};
+				mySkills = new Skill[] {new SKILL_BasicAttack(), new SKILL_Guard(), new SKILL_IronWave(), new SKILL_Taunt()};
 				
 				myArmor = new ArmorItem(Chance.choose(allowedArmors), "null");
 				myWeapon = new WeaponItem(Chance.choose(new String[] {"greatsword","shortsword","polearm"}), "null");
---
+++ src/SKILL_Taunt.java
@@ -0,0 +1,22 @@
+
+public class SKILL_Taunt extends Skill {
+	public SKILL_Taunt () {
+		super(15);
+		setName("Taunt");
+		setDescription("Heal 50 HP. All enemies lose 20 strength and will target the character who uses this skill next turn.");
+		setvTarget("NA");
+	}
+	
+	public void activationEffect (Entity me, Entity target) {
+		CombatPane currentBattle = MainApplication.combatPane;
+		for (Enemy a : currentBattle.aliveEnemies()) {
+			a.getStatSpread()[EntityStats.STR.ordinal()] -= 20;
+			a.getStatSpread()[EntityStats.DEX.ordinal()] -= 20;
+			a.setNextTarget((Character)me);
+		}
+		
+		me.heal(50);
+		
+		me.drainMana(getManaCost());
+	}
+}
