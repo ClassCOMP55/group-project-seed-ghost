f3847bc14ac1bcc55c54184c12373f27b3320f15
Isaac Ochoa <i_ochoa3@u.pacific.edu>
2026-04-12 11:48:55 -0700

    Made a displayInventory() method

--- src/MapPane.java
+++ src/MapPane.java
@@ -14,8 +14,13 @@ public class MapPane extends GraphicsPane {
 	
 	private ArrayList<Node> mapPath;
 	private ArrayList<GObject> myNodeObjects;
+	private ArrayList<GRect> extraArmorButtons,extraWeaponButtons,consumablesButtons,extra;
+	private ArrayList<GLabel> extraArmorLabels,extraWeaponLabel,consumablesLabel,extraLabels;
+	private boolean forWeapon,forArmor,forConsumable;
 	public static Node currPosition;
 	int count;
+	GRect inventoryButton;
+	GLabel inventoryButtonLabel;
 	
 	public MapPane(MainApplication mainScreen) {
 		this.mainScreen = mainScreen;
@@ -26,9 +31,14 @@ public class MapPane extends GraphicsPane {
 	
 	@Override
 	public void showContent() {
+		extraWeaponButtons = new ArrayList<>();
+		extraWeaponLabel = new ArrayList<>();
+		extra = new ArrayList<>();
+		extraLabels = new ArrayList<>();
 		myNodeObjects = new ArrayList<>();
 		createBackground();
 		addText();
+		addButtons();
 		createMap();
 	}
 
@@ -56,6 +66,157 @@ public class MapPane extends GraphicsPane {
 		contents.add(title);
 		mainScreen.add(title);
 	}
+	
+	private void addButtons() {
+		double buttonHeight = (MainApplication.WINDOW_HEIGHT/10.0);
+		double buttonWidth = MainApplication.WINDOW_WIDTH*(200.0/800.0);
+		
+		inventoryButton = new GRect(buttonWidth,buttonHeight);
+		inventoryButton.setFillColor(Color.DARK_GRAY);
+		inventoryButton.setFilled(true);
+		inventoryButton.setLocation(MainApplication.WINDOW_WIDTH-buttonWidth,MainApplication.WINDOW_HEIGHT-buttonHeight);
+		contents.add(inventoryButton);
+		mainScreen.add(inventoryButton);
+		
+		inventoryButtonLabel = new GLabel("Inventory");
+		inventoryButtonLabel.setLocation(inventoryButton.getX()+(buttonWidth-inventoryButtonLabel.getWidth())/2, inventoryButton.getY()+(buttonHeight-inventoryButtonLabel.getHeight())/2+inventoryButtonLabel.getHeight());
+		contents.add(inventoryButtonLabel);
+		mainScreen.add(inventoryButtonLabel);
+	}
+	
+	private void displayInventory() {
+		
+		double buttonHeight = (MainApplication.WINDOW_HEIGHT/10.0);
+		double buttonWidth = MainApplication.WINDOW_WIDTH*(200.0/800.0);
+		double allBoxWidth = (buttonWidth+20)*3;
+		
+		PlayerInventory inventory = CharacterSelectionPane.myInventory;
+		ArrayList<WeaponItem> weapons = inventory.getExtraWeapons();
+		ArrayList<ArmorItem> armors = inventory.getExtraArmors();
+		ConsumableItem[] consumables = inventory.getConsumables();
+		WeaponItem testWeapon = new WeaponItem(true);
+		weapons.add(testWeapon);
+		ArmorItem testArmor = new ArmorItem(true);
+		armors.add(testArmor);
+		
+		GRect box1 = new GRect(buttonWidth+20,(buttonHeight*(weapons.size()+2))+20);
+		box1.setLocation((MainApplication.WINDOW_WIDTH-allBoxWidth)/2+(box1.getWidth()*0), (MainApplication.WINDOW_HEIGHT-box1.getHeight())/2);
+		box1.setFilled(true);
+		box1.setColor(Color.BLUE);
+		contents.add(box1);
+		mainScreen.add(box1);
+		extra.add(box1);
+		
+		GRect extraRect = new GRect(buttonWidth,buttonHeight);
+		extraRect.setLocation(box1.getX()+10, box1.getY()+10);
+		extraRect.setFilled(true);
+		extraRect.setFillColor(Color.DARK_GRAY);
+		contents.add(extraRect);
+		mainScreen.add(extraRect);
+		extra.add(extraRect);
+		
+		GLabel extraLabel = new GLabel("Extra Weapons");
+		extraLabel.setLocation(extraRect.getX()+(extraRect.getWidth()-extraLabel.getWidth())/2, extraRect.getY()+(extraRect.getHeight()-extraLabel.getHeight())/2+extraLabel.getHeight());
+		contents.add(extraLabel);
+		mainScreen.add(extraLabel);
+		extraLabels.add(extraLabel);
+		
+		for (int i = 0;i<weapons.size();i++) {
+			GRect button = new GRect(buttonWidth,buttonHeight);
+			button.setLocation(extraRect.getX(), (extraRect.getY()+extraRect.getHeight())+(i*buttonHeight));
+			button.setFilled(true);
+			button.setFillColor(Color.DARK_GRAY);
+			contents.add(button);
+			mainScreen.add(button);
+			extraWeaponButtons.add(button);
+			
+			GLabel label = new GLabel(weapons.get(i).toString()+" (Affinity:"+weapons.get(i).getAffinity()+")");
+			label.setLocation(button.getX()+(button.getWidth()-label.getWidth())/2, button.getY()+(button.getHeight()-label.getHeight())/2+label.getHeight());
+			contents.add(label);
+			mainScreen.add(label);
+			extraWeaponLabel.add(label);
+		}
+		
+		
+		
+		box1 = new GRect(buttonWidth+20,(buttonHeight*(armors.size()+2))+20);
+		box1.setLocation((MainApplication.WINDOW_WIDTH-allBoxWidth)/2+(box1.getWidth()*1), (MainApplication.WINDOW_HEIGHT-box1.getHeight())/2);
+		box1.setFilled(true);
+		box1.setColor(Color.RED);
+		contents.add(box1);
+		mainScreen.add(box1);
+		extra.add(box1);
+		
+		extraRect = new GRect(buttonWidth,buttonHeight);
+		extraRect.setLocation(box1.getX()+10, box1.getY()+10);
+		extraRect.setFilled(true);
+		extraRect.setFillColor(Color.DARK_GRAY);
+		contents.add(extraRect);
+		mainScreen.add(extraRect);
+		extra.add(extraRect);
+		
+		extraLabel = new GLabel("Extra Armors");
+		extraLabel.setLocation(extraRect.getX()+(extraRect.getWidth()-extraLabel.getWidth())/2, extraRect.getY()+(extraRect.getHeight()-extraLabel.getHeight())/2+extraLabel.getHeight());
+		contents.add(extraLabel);
+		mainScreen.add(extraLabel);
+		extraLabels.add(extraLabel);
+		
+		for (int i = 0;i<armors.size();i++) {
+			GRect button = new GRect(buttonWidth,buttonHeight);
+			button.setLocation(extraRect.getX(), (extraRect.getY()+extraRect.getHeight())+(i*buttonHeight));
+			button.setFilled(true);
+			button.setFillColor(Color.DARK_GRAY);
+			contents.add(button);
+			mainScreen.add(button);
+			extraWeaponButtons.add(button);
+			
+			GLabel label = new GLabel(armors.get(i).toString()+" (Affinity:"+armors.get(i).getAffinity()+")");
+			label.setLocation(button.getX()+(button.getWidth()-label.getWidth())/2, button.getY()+(button.getHeight()-label.getHeight())/2+label.getHeight());
+			contents.add(label);
+			mainScreen.add(label);
+			extraWeaponLabel.add(label);
+		}
+		
+		box1 = new GRect(buttonWidth+20,(buttonHeight*(consumables.length+2))+20);
+		box1.setLocation((MainApplication.WINDOW_WIDTH-allBoxWidth)/2+(box1.getWidth()*2), (MainApplication.WINDOW_HEIGHT-box1.getHeight())/2);
+		box1.setFilled(true);
+		box1.setColor(Color.GREEN);
+		contents.add(box1);
+		mainScreen.add(box1);
+		extra.add(box1);
+		
+		extraRect = new GRect(buttonWidth,buttonHeight);
+		extraRect.setLocation(box1.getX()+10, box1.getY()+10);
+		extraRect.setFilled(true);
+		extraRect.setFillColor(Color.DARK_GRAY);
+		contents.add(extraRect);
+		mainScreen.add(extraRect);
+		extra.add(extraRect);
+		
+		extraLabel = new GLabel("Consumables");
+		extraLabel.setLocation(extraRect.getX()+(extraRect.getWidth()-extraLabel.getWidth())/2, extraRect.getY()+(extraRect.getHeight()-extraLabel.getHeight())/2+extraLabel.getHeight());
+		contents.add(extraLabel);
+		mainScreen.add(extraLabel);
+		extraLabels.add(extraLabel);
+		
+		for (int i = 0;i<consumables.length;i++) {
+			GRect button = new GRect(buttonWidth,buttonHeight);
+			button.setLocation(extraRect.getX(), (extraRect.getY()+extraRect.getHeight())+(i*buttonHeight));
+			button.setFilled(true);
+			button.setFillColor(Color.DARK_GRAY);
+			contents.add(button);
+			mainScreen.add(button);
+			extraWeaponButtons.add(button);
+			
+			GLabel label = new GLabel(consumables.toString());
+			label.setLocation(button.getX()+(button.getWidth()-label.getWidth())/2, button.getY()+(button.getHeight()-label.getHeight())/2+label.getHeight());
+			contents.add(label);
+			mainScreen.add(label);
+			extraWeaponLabel.add(label);
+		}
+	}
+	
+	
 	
 private void createMap() {
 	count =0;
@@ -149,6 +310,9 @@ private void createMap() {
 		GObject obj = mainScreen.getElementAtLocation(e.getX(), e.getY());
 		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(1)) {
 			mainScreen.switchToShopPane();
+		}
+		else if (obj == inventoryButton) {
+			displayInventory();
 		}
 		if (myNodeObjects.contains(obj)) {
 			GObject oval = mainScreen.getElementAtLocation(e.getX(), e.getY());
