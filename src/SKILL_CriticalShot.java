09a757c86ada4fab28fd0e2a09387272beb28dfe
cFranks2 <c_franks2@u.pacific.edu>
2026-04-21 11:28:38 -0700

    Critical shot buff

--- src/SKILL_CriticalShot.java
+++ src/SKILL_CriticalShot.java
@@ -10,8 +10,8 @@ public class SKILL_CriticalShot extends Skill {
 	
 	public void activationEffect (Entity me, Entity target) {
 		double[] dmg = new double[] {0,0,0,0,0,0,0,0};
-		int scaling = (int)Math.round(me.getStatSpread()[EntityStats.PRC.ordinal()]*0.4 + me.getStatSpread()[EntityStats.DEX.ordinal()]*0.4);
-		double damage = 10 * Math.pow(1.06, scaling);
+		int scaling = (int)Math.round(me.getStatSpread()[EntityStats.PRC.ordinal()]*0.7 + me.getStatSpread()[EntityStats.DEX.ordinal()] * 0.3);
+		double damage = 10 * Math.pow(1.07, scaling);
 		dmg[DamageType.PIERCE.ordinal()] = damage * 0.5;
 		dmg[DamageType.BLAST.ordinal()] = damage * 0.5;
 		target.attackMe(dmg);
@@ -20,7 +20,7 @@ public class SKILL_CriticalShot extends Skill {
 	}
 	
 	public String getEnemyIntentMsg(Entity me, Entity target) {
-		int scaling = (int)Math.round(me.getStatSpread()[EntityStats.PRC.ordinal()]*0.8 + me.getStatSpread()[EntityStats.DEX.ordinal()]*0.6);
-		return "Intends to shoot the " + target + " for " + ((Character)target).calculateDamage(10 * Math.pow(1.05, scaling), DamageType.PIERCE) + " damage!";
+		int scaling = (int)Math.round(me.getStatSpread()[EntityStats.PRC.ordinal()]*0.7 + me.getStatSpread()[EntityStats.DEX.ordinal()] * 0.3);
+		return "Intends to shoot the " + target + " for " + ((Character)target).calculateDamage(10 * Math.pow(1.07, scaling), DamageType.PIERCE) + " damage!";
 	}
 }
